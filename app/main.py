eba9e915cdddea2137c9d9d774683da152d57774
Christian Rios <criosmancilla@gmail.com>
2025-11-04 21:16:45 -0500

    Adding Api prefix and basic config

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
+# app/main.py
 from fastapi import FastAPI
+from app.core.config import settings
 
 app = FastAPI(title="Secure Healthcare Appointment System")
 
 @app.get("/")
 def root():
-    return {"message": "Healthcare System API running successfully"}
+    return {"message": "Healthcare System API running successfully", "env": settings.APP_ENV}
