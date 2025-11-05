2c789600cd95aec322b71f8919a1013ba240c67d
chandiler <lan767228@gmail.com>
2025-11-04 20:37:57 -0500

    Update dependencies in requirements.txt

---
+++ config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+# 只加载一次环境变量（整个项目都会共享）
+load_dotenv()
+
+DB_CONFIG = {
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT")
+}
